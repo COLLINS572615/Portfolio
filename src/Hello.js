8d72aa27062bdbc1f7158645908060eacbe78b0a
amos-kibet <kibetamos97@gmail.com>
2022-09-14 23:24:02 +0300

    experiement child component creation

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Button } from "react-bootstrap";
+
+function Hello() {
+  return (
+    <Container>
+      <h2>Hello React, Amos Kibet here, from a child component (Hello)</h2>
+      <Button>Button</Button>
+    </Container>
+  );
+}
+
+export default Hello;
