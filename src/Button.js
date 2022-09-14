63c42f1c0fba193a0c27a58926a7d65141783ab8
amos-kibet <kibetamos97@gmail.com>
2022-09-14 23:24:09 +0300

    experiement child component creation

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+// @ts-nocheck
+import React from "react";
+import { Button } from "react-bootstrap";
+
+function Btn() {
+  return (
+    <div>
+      <Button className="fork-btn-inner">❌</Button>
+    </div>
+  );
+}
+
+export default Btn;
